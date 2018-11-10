122a407425590d18937ffbaa5b6e069db3595761
wanyuaning <wanyuaning@163.com>
2018-11-11 01:57:59 +0800

    first

---
+++ src/001.js
@@ -0,0 +1,5 @@
+
+const b = '模块001'
+console.log(b)
+// export import 用法请参考es6项目
+export default b
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import name from './001'
+
+let a = '入口文件'
+console.log(a)
+console.log(name)
+// export import 用法请参考es6项目
+export const Lib = name
